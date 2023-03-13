c2607c9b956ca13c25f58a8ea7d29ba40de29df4
Adhham Safwan <msmadhhamg@gmail.com>
2023-03-13 22:48:45 +0000

    Add discover icon

--- airflix/src/App.js
+++ airflix/src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
+import logo from './images/logo.jpg'
+import profile from './images/profile.jpg'
+import Lottie from 'lottie-react'
+import discoverIcon from './lotties/discover.json'
 
 function App() {
   return (
     <div className="App">
-      
+      <header>
+        <img className='logo' src={logo} alt="Airflix logo" role='banner' />
+        <img className='profile' src={profile} alt="User Profie" />
+      </header>
+      <nav>
+        <a href="/" role='button'><Lottie animationData={discoverIcon} autoplay={true} style={{width: 60}} /></a>
+        <a href="#" role='button'></a>
+        <a href="#" role='button'></a>
+        <a href="#" role='button'></a>
+        <a href="#" role='button'></a>
+        <a href="#" role='button'></a>
+      </nav>
     </div>
   );
 }
