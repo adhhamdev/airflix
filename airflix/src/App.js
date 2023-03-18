8eba9d2c30b7dd075f7d7a3d8038cf6f34812d09
AdhhamDev <msmadhhamg@gmail.com>
2023-03-18 20:05:07 +0530

    add nav and icons

--- airflix/src/App.js
+++ airflix/src/App.js
@@ -6,16 +6,19 @@ function App() {
   return (
     <div className="App">
       <header>
-        <img className='logo' src={logo} alt="Airflix logo" role='banner' />
-        <img className='profile' src={profile} alt="User Profie" />
+        <a href="/"><img className='logo' src={logo} alt="Airflix logo" role='banner' /></a>
+        <div className="userActions">
+          <img className='profile' src={profile} alt="User Profie" />
+          <i className="fas fa-life-ring"></i>
+        </div>
       </header>
       <nav>
-        <a href="/" role='button'></a>
-        <a href="#" role='button'></a>
-        <a href="#" role='button'></a>
-        <a href="#" role='button'></a>
-        <a href="#" role='button'></a>
-        <a href="#" role='button'></a>
+        <a href="/" role='button'><i className="fas fa-compass"></i></a>
+        <a href="#" role='button'><i className="fas fa-search"></i></a>
+        <a href="#" role='button'><i className="fas fa-play"></i></a>
+        <a href="#" role='button'><i className="fas fa-list-ul"></i></a>
+        <a href="#" role='button'><i className="fab fa-heart"></i></a>
+        <a href="#" role='button'><i className="fas fa-lines-leaning"></i></a>
       </nav>
     </div>
   );
