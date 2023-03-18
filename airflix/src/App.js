af9235f4bd587be8f359e4dc9298201b8cc41550
AdhhamDev <msmadhhamg@gmail.com>
2023-03-18 19:04:11 +0530

    remove lottiefiles

--- airflix/src/App.js
+++ airflix/src/App.js
@@ -1,8 +1,6 @@
 import './App.css';
 import logo from './images/logo.jpg'
 import profile from './images/profile.jpg'
-import Lottie from 'lottie-react'
-import discoverIcon from './lotties/discover.json'
 
 function App() {
   return (
@@ -12,7 +10,7 @@ function App() {
         <img className='profile' src={profile} alt="User Profie" />
       </header>
       <nav>
-        <a href="/" role='button'><Lottie animationData={discoverIcon} autoplay={true} style={{width: 60}} /></a>
+        <a href="/" role='button'></a>
         <a href="#" role='button'></a>
         <a href="#" role='button'></a>
         <a href="#" role='button'></a>
