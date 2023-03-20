ecf318602f016f2817866cf5c4fd6ad52f34b760
Adhham Safwan <msmadhhamg@gmail.com>
2023-03-21 02:24:04 +0530

    remove subfolder and unused files

--- src/App.js
+++ src/App.js
@@ -1,27 +1,39 @@
-import './App.css';
-import logo from './images/logo.jpg'
-import profile from './images/profile.jpg'
+import logo from "./images/logo.jpg";
+import profile from "./images/profile.jpg";
 
 function App() {
   return (
     <div className="App">
       <header>
-        <a href="/"><img className='logo' src={logo} alt="Airflix logo" role='banner' /></a>
+        <a href="/">
+          <img className="logo" src={logo} alt="Airflix logo" role="banner" />
+        </a>
         <div className="userActions">
-          <img className='profile' src={profile} alt="User Profie" />
+          <img className="profile" src={profile} alt="User Profie" />
           <i className="fas fa-life-ring"></i>
         </div>
       </header>
       <nav>
-        <a href="/" role='button'><i className="fas fa-compass"></i></a>
-        <a href="#" role='button'><i className="fas fa-search"></i></a>
-        <a href="#" role='button'><i className="fas fa-play"></i></a>
-        <a href="#" role='button'><i className="fas fa-list-ul"></i></a>
-        <a href="#" role='button'><i className="fab fa-heart"></i></a>
-        <a href="#" role='button'><i className="fas fa-lines-leaning"></i></a>
+        <a href="/" role="button">
+          <i className="fas fa-compass"></i>
+        </a>
+        <a href="#" role="button">
+          <i className="fas fa-search"></i>
+        </a>
+        <a href="#" role="button">
+          <i className="fas fa-play"></i>
+        </a>
+        <a href="#" role="button">
+          <i className="fas fa-list-ul"></i>
+        </a>
+        <a href="#" role="button">
+          <i className="fab fa-heart"></i>
+        </a>
+        <a href="#" role="button">
+          <i className="fas fa-lines-leaning"></i>
+        </a>
       </nav>
     </div>
   );
 }
-
 export default App;
