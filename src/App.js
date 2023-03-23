28efb9f43f38125586c4b6d22601e559fd4c36e3
Adhham Safwan <msmadhhamg@gmail.com>
2023-03-24 01:30:50 +0530

    Add nav style

--- src/App.js
+++ src/App.js
@@ -10,30 +10,30 @@ function App() {
           <img className="logo" src={logo} alt="Airflix logo" role="banner" />
         </a>
         <div className="userActions">
-          <img className="profile" src={profile} alt="User Profie" />
-          <i className="fas fa-life-ring"></i>
+          <nav>
+            <a href="/" role="button" data-tooltip="Discover">
+              <i className="fas fa-compass"></i>
+            </a>
+            <a href="#" role="button" data-tooltip="Search">
+              <i className="fas fa-search"></i>
+            </a>
+            <a href="#" role="button" data-tooltip="Watchlist">
+              <i className="fas fa-play"></i>
+            </a>
+            <a href="#" role="button" data-tooltip="Lists">
+              <i className="fas fa-list-ul"></i>
+            </a>
+            <a href="#" role="button" data-tooltip="Favorites">
+              <i className="fas fa-heart"></i>
+            </a>
+            <a href="#" role="button" data-tooltip="Collections">
+              <i className="fas fa-lines-leaning"></i>
+            </a>
+          </nav>
+          <img className="profile" src={profile} alt="User Profie" data-tooltip="Profile"/>
+          <i className="fas fa-life-ring support" data-tooltip="Support"></i>
         </div>
       </header>
-      <nav>
-        <a href="/" role="button">
-          <i className="fas fa-compass"></i>
-        </a>
-        <a href="#" role="button">
-          <i className="fas fa-search"></i>
-        </a>
-        <a href="#" role="button">
-          <i className="fas fa-play"></i>
-        </a>
-        <a href="#" role="button">
-          <i className="fas fa-list-ul"></i>
-        </a>
-        <a href="#" role="button">
-          <i className="fab fa-heart"></i>
-        </a>
-        <a href="#" role="button">
-          <i className="fas fa-lines-leaning"></i>
-        </a>
-      </nav>
     </div>
   );
 }
