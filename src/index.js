a57685932975614d18b7b6e0ebacac30273eeae9
Adhham Safwan <msmadhhamg@gmail.com>
2023-04-02 15:18:44 +0530

    authenticate: api to www

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ const router = createBrowserRouter([
           const login = await (await fetch(`https://api.themoviedb.org/authenticate/${requestToken.request_token}`)).json();
           console.log(login);
 
-          const session = await (await fetch('https://api.themoviedb.org/3/authentication/session/new?api_key=08a7337c36b62d4a8a9dfafd26b3afb6', {
+          const session = await (await fetch('https://www.themoviedb.org/3/authentication/session/new?api_key=08a7337c36b62d4a8a9dfafd26b3afb6', {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json'
