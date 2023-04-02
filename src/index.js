47e0de686ebfb511f9381cbe778f65a8453f0712
Adhham Safwan <msmadhhamg@gmail.com>
2023-04-02 15:02:31 +0530

    create user session

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ const router = createBrowserRouter([
           console.log(requestToken);
 
           /* request the user to login */
-          const login = await (await fetch(`https://www.themoviedb.org/authenticate/${requestToken.request_token}`)).json();
+          const login = await (await fetch(`https://api.themoviedb.org/authenticate/${requestToken.request_token}`)).json();
           console.log(login);
 
           const session = await (await fetch('https://api.themoviedb.org/3/authentication/session/new?api_key=08a7337c36b62d4a8a9dfafd26b3afb6', {
