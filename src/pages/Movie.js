41fece8a470688d8e44501dd77785f104aeed7d2
Adhham Safwan <msmadhhamg@gmail.com>
2023-05-19 18:56:26 +0530

    Add more movie details

--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -4,6 +4,7 @@ import { useState } from "react";
 
 const Movie = () => {
     const { movie, keywords, similars, videos, watchProviders, credits, recommendations } = useLoaderData();
+    console.log(recommendations)
     const [starBtn, setStarBtn] = useState({ text: "RATE IT", icon: "far fa-star" });
     const imgBaseURL = "https://image.tmdb.org/t/p/w500";
     window.onscroll = () => {
@@ -72,36 +73,65 @@ const Movie = () => {
                         })}
                     </div>
                 </div>
-                {watchProviders.length > 0 &&
-                    <div className="watchProviders">
-                        <h2>WATCH PROVIDERS</h2>
-                        <div className="watchProvidersList">
-                            {(watchProviders.find(provider => provider[0] == 'US')[1].flatrate || watchProviders.find(provider => provider[0] == 'US')[1].buy).map((provider) => {
+                <div className="watchProviders">
+                    <h2>WATCH PROVIDERS</h2>
+                    {watchProviders.length ? <div className="watchProvidersList">
+                        {(watchProviders.find(provider => provider[0] == 'US' || provider[0] == 'AT')[1].flatrate || watchProviders.find(provider => provider[0] == 'US' || provider[0] == 'AT')[1].buy).map((provider) => {
+                            return (
+                                <div key={provider.provider_name} className="watchProvider">
+                                    <img className="providerLogo" src={`https://www.themoviedb.org/t/p/original${provider.logo_path}`} alt={provider.provider_name} />
+                                    <p className="providerName">{provider.provider_name}</p>
+                                </div>
+                            )
+                        })}
+                    </div> : <p className="noProviders">No providers found...</p>}
+                </div>
+                <div className="credits">
+                    <h2>CREDITS</h2>
+                    <div className="creditsList">
+                        <div className="casts">
+                            <h3>CAST</h3>
+                            {credits.map((cast) => {
                                 return (
-                                    <div key={provider.provider_name} className="watchProvider">
-                                        <img className="providerLogo" src={`https://www.themoviedb.org/t/p/original${provider.logo_path}`} alt={provider.provider_name} />
-                                        <p className="providerName">{provider.provider_name}</p>
+                                    <div key={cast.cast_id} className="castMember">
+                                        {cast.profile_path ? <img src={imgBaseURL + cast.profile_path} alt={cast.name} /> : <div className="castMemberImgFB"></div>}
+                                        <p className="castMemberName" title="NAME">{cast.name}</p>
+                                        <p className="castMemberCharacter" title="CHARACTER">{cast.character}</p>
                                     </div>
                                 )
                             })}
                         </div>
-                    </div>}
-                <div className="videos"> 
-                    <h2>VIDEOS</h2>
-                    {videos.map((video) => {
-                        return (
-                            <div key={video.id} className="video">
-                                <iframe loading="lazy" width="560" height="315" src={`https://www.youtube.com/embed/${video.key}`} title={video.name} frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-                                <p className="videoName">{video.name}</p>
-                            </div>
-                        )
-                    })}
+                    </div>
                 </div>
-                <div className="credits">
-
+                <div className="videos"> 
+                    <details>
+                        <summary>VIDEOS</summary>
+                        {videos.length ? videos.map((video) => {
+                            return (
+                                <div key={video.id} className="video">
+                                    <iframe loading="lazy" width="560" height="315" src={`https://www.youtube.com/embed/${video.key}`} title={video.name} frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                                    <p className="videoName">{video.name}</p>
+                                </div>
+                            )
+                        }) : <p className="noVideos">No videos found...</p>}
+                    </details>
                 </div>
                 <div className="recommendations">
-
+                    <h2>RECOMMENDATIONS</h2>
+                    <div className="recommendationsList">
+                        {recommendations.length ? recommendations.map((movie) => {
+                            return (
+                                <Link to={`../movie/${movie.id}`} key={movie.id} className="recommendedMovie">
+                                    {movie.backdrop_path ? <img className="recommendedTN" src={imgBaseURL + movie.backdrop_path} alt="Movie Backdrop" /> : <div className="recommendedTNFallback"></div>}
+                                    <div className="recommendedMovieInfo">
+                                        <p className="recommendedMovieTitle">{movie.title}</p>
+                                        <p className="recommendedMovieRating"><i className="fas fa-star"></i> {movie.vote_average}</p>
+                                        <p className="recommendedMovieLang"><i className="fas fa-language"></i> {movie.original_language}</p>
+                                    </div>
+                                </Link>
+                            )
+                            }) : <p className="noRecommendations">No recommendations found...</p>}
+                    </div>
                 </div>
             </main>
             <aside className="similarMovies">
