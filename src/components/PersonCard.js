b7fa4cf9145c28bcb3be138754b8bc40bdc4ee57
Adhham Safwan <msmadhhamg@gmail.com>
2024-09-01 03:00:20 +0530

    fix minor design bugs

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -1,17 +1,19 @@
 const PersonCard = ({ original_name, profile_path, popularity }) => {
-  const imgBaseURL = "https://image.tmdb.org/t/p/w500";
+  const imgBaseURL = 'https://image.tmdb.org/t/p/w500';
   const imgSrc = imgBaseURL + profile_path;
   return (
-    <div className="PersonCard">
-      <img src={imgSrc} alt={original_name} loading="lazy" />
-      <div className="info">
-        <p className="name">{original_name}</p>
-        <p className="popularity">
-          <i className="fas fa-star"></i> {popularity}
+    <div className='PersonCard'>
+      <div className='person-image'>
+        <img src={imgSrc} alt={original_name} loading='lazy' />
+      </div>
+
+      <div className='info'>
+        <h3 className='name'>{original_name}</h3>
+        <p className='popularity'>
+          <i className='fas fa-users'></i> {popularity.toFixed(1)}
         </p>
       </div>
     </div>
   );
 };
-
 export default PersonCard;
